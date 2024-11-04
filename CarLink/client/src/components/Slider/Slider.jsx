d4b8e33cb0c8b806e18f333859f86d29b091a4a7
Phu641 <phamphuhoa203@gmail.com>
2024-11-04 19:21:51 +0700

    update homepage v3

--- CarLink/client/src/components/Slider/Slider.jsx
+++ CarLink/client/src/components/Slider/Slider.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import styles from "./Slider.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-import backgroundImage from '/background.png';
+import backgroundImage from '/background4.png';
 
 const Slider = () => {
-
   return (
     <section className={styles.sliderWrapper}>
-        <div className={styles.sliderContainer}
+      <div
+        className={styles.sliderContainer}
         style={{
           backgroundImage: `url(${backgroundImage})`,
-          height: '800px',
-          
+          height: '500px',
           backgroundSize: "cover",
           backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
-        >
-
-       <div className={styles.paddings}>
-          <SearchBar/>
-       </div>
-    </div>
+      >
+        <div className={styles.paddings}>
+          <SearchBar />
+          <div className="primaryText"
+            style={{
+              color: "white",
+              textAlign: "center"
+            }}
+          >Hành trình của bạn luôn có CarLink đồng hành!</div>
+        </div>
+      </div>
     </section>
   );
 };
